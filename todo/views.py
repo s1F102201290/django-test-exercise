bbf31f9a7ccbd7af48a232d4958ed5ab6b8b2395
1f10220123 <s1f102201234@iniad.org>
2023-07-13 14:05:52 +0900

    投稿の削除機能の追加

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import Http404
 from django.utils.timezone import make_aware
 from django.utils.dateparse import parse_datetime
@@ -30,8 +30,17 @@ def detail(request, task_id):
         task = Task.objects.get(pk=task_id)
     except Task.DoesNotExist:
         raise Http404("Task does not exist")
-    
+
     context = {
-        'task': task,        
+        'task': task,
     }
     return render(request, 'todo/detail.html', context)
+
+
+def delete(request, task_id):
+    try:
+        task = Task.objects.get(pk=task_id)
+    except Task.DoesNotExist:
+        raise Http404("task does not exist")
+    task.delete()
+    return redirect(index)
